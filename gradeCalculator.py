9857276f51619edc65aa0552f8f9e7f07f9fa6c9
Daniel Fitzpatrick <Daniel.Fitzpatrick@vu.com>
2023-01-30 14:10:39 -0600

    scoring system

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,8 +2,25 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
+bad_score_message: "Bad Score"
+try:
+    score = float (input("Enter score:"))
+    if score < 0.6: 
+        grade = "F"
+    elif score >= .61 and score <=.69:
+        grade = "D"
+    elif score >= 0.7 and score <= 0.79:
+        grade = "C"
+    elif score >= 0.8 and hrs <= 0.89:
+        grade = "B"
+    elif hrs >= 0.9 and  hrs < 1.00:
+        grade = "A"
+    else:
+        grade = "Bad Score"
+except:
+        grade = "Bad Score"
 
-    hrs = float(input("Enter score:"))
+print(grade)
 
     # end assignment
 
